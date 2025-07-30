4bfef1131fa75c63b7673d92e604f7caaa787b41
Malik Elmessiry <malik.messiry@gmail.com>
2025-07-30 16:12:42 +0300

    Remove unused logout functionality from Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,6 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
-    const navigate = useNavigate();
-
-    const handleLogout = () => {
-        localStorage.removeItem('authToken');
-        navigate('/auth');
-    };
 
     return (
         <nav className='navbar'>
